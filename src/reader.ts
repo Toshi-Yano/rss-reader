480431bda03c2cc453f57c71d2032bfd5e0a13a6
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-14 22:58:09 +0900

    [change]特定のURLからRSSを取得するクラスを作成

---
+++ src/reader.ts
@@ -0,0 +1,30 @@
+import Parser from 'rss-parser';
+import { RSSReader } from './interfaces';
+import { Channel, Item } from './types';
+
+export class Reader implements RSSReader {
+  private readonly parser: Parser<Channel, Item>;
+
+  constructor(private subscribeUrls: ReadonlyArray<string>) {
+    // rss-parserのOutput, Item型に定義されていない要素を正確な名称で取得するため、customFieldsに定義を追加
+    // 購読するRSSが増え対象項目が増加した際は定義の追加が必要
+    this.parser = new Parser({
+      customFields: {
+        feed: ['lastBuildDate', 'docs', 'generator'],
+        item: ['description'],
+      },
+    });
+  }
+
+  /**
+   * 対象URLからRSSを取得し、Channel[]型へ変換して返却する
+   * @returns 全てのURLを元に取得した、Itemを子に持つChannel
+   */
+  async loadParsedRSS() {
+    const channels: Channel[] = [];
+    for (const url of this.subscribeUrls) {
+      channels.push((await this.parser.parseURL(url)) as Channel);
+    }
+    return channels;
+  }
+}
