8a5f85b7d787764e13c7883974eff3c3337a34d0
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-15 23:46:11 +0900

    [change]Convertorの呼び出しを追加

--- src/app.ts
+++ src/app.ts
@@ -4,26 +4,32 @@ import { RSSReader, Consumer } from './interfaces';
 import { Convertor } from './convertor';
 
 const subscribeUrls = ['https://tech.uzabase.com/rss'];
+// const subscribeUrls = ['http://www.openspc2.org/RSS/Atom/link/sample.xml'];
 const excludeWords = ['NewsPicks'];
 
 const readRSS = async (
   reader: RSSReader,
-  convertor: Convertor,
+  convertor?: Convertor,
   consumer?: Consumer,
 ) => {
   const channels = await reader.loadParsedRSS();
   // console.log({ channels });
+  convertor &&
+    channels.forEach((channel) => {
+      convertor.executes(channel);
+      // consumer?.();
+    });
 };
 
 const readRSSFromURLs = async (urls: ReadonlyArray<string>) => {
   const reader = new Reader(urls);
-  const convertors = [new WordsExcluder(excludeWords)];
-  const convertor = new Convertor(convertors);
+  const convertor = new Convertor([
+    new WordsExcluder(excludeWords),
+    // new WordsExcluder(['SPEEDA', 'ユーザベース', 'uzabase', 'blogs']),
+  ]);
+  // const consumer = new Consumer([new StdOuter()]);
 
   readRSS(reader, convertor);
-  // const consumer = new StdOuter();
-  // consumer.executes();
-  // }
 };
 
 readRSSFromURLs(subscribeUrls);
