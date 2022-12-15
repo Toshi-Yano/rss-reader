920a5050fc5adca14af317949f774bd735ad1d2d
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-16 02:24:16 +0900

    [fix]itemsが空になる不具合を修正

--- src/convertor.ts
+++ src/convertor.ts
@@ -12,20 +12,23 @@ export class Convertor {
     // console.log('--------------------------');
     // console.log({ item });
 
-    // items.forEach((i) => {
-    //   console.log(i);
+    // console.log('-------------------------');
+    // let count = 0;
+    // for (let i = 0; i < 2; i++) {
+    //   console.log(channel.items[i]);
+    // }
+    // channel.items.forEach((item) => {
+    //   console.log(item);
     // });
+
+    // console.log('--------------------------');
+    // this.iterateElementInArray(channel.items, channel, 'items');
   }
 
   mightConvertRecursively(element: Channel | Item | Enclosure) {
-    // console.log(element);
     if (element === undefined || element === null) return;
 
     for (let [key, value] of Object.entries(element)) {
-      // Item型は別メソッドでconvertするため処理をスキップ
-      // if (key === 'items') continue;
-      // if (Array.isArray(value)) continue;
-
       if (typeof value === 'string') {
         element[key] = this.useAllConvertor(value);
       } else if (Array.isArray(value)) {
@@ -41,10 +44,33 @@ export class Convertor {
     element: Channel | Item | Enclosure,
     key: string,
   ) {
+    // console.log(`${key} : ${value} : ${typeof value[0]}`);
+    // if (key === 'items') {
+    //   console.log(`${value} : ${typeof value[0]}`);
+    //   console.log(Array.isArray(value));
+    // }
+
     if (value.length === 0) return;
-    if (typeof value[0] !== 'string') this.mightConvertRecursively(value);
+    // if (typeof value[0] !== 'string') this.mightConvertRecursively(value);
+    if (typeof value[0] !== 'string') {
+      value.forEach((v) => {
+        this.mightConvertRecursively(v as Item);
+      });
+      return;
+      // if (key === 'items') {
+      //   console.log('fdjkafjalk;');
+      //   // element[key] =
+      //   value.forEach((v) => {
+      //     this.mightConvertRecursively(v as Item);
+      //   });
+      //   return;
+      // }
+      // this.mightConvertRecursively(value);
+    }
 
     const stringArray = value.filter((v) => typeof v === 'string') as string[];
+    // console.log(stringArray);
+
     element[key] = this.useAllConvertor(stringArray);
   }
 
