14243025b1b678b08080085e0812b3a7741dd34b
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-17 01:04:32 +0900

    [change]コメント追加

--- src/words-excluder.ts
+++ src/words-excluder.ts
@@ -6,9 +6,15 @@ const URL_REGEX =
 export class WordsExcluder implements Convertable {
   private readonly excludedWordsRegex: RegExp;
   constructor(private words: ReadonlyArray<string>) {
+    // 単語の区切りを無視したグローバルサーチ
     this.excludedWordsRegex = new RegExp(`(${this.words.join('|')})`, 'g');
   }
 
+  /**
+   * URL以外の文字列から、対象の単語を全て除外して返却する
+   * @param value 変換検証対象の値
+   * @returns     単語除外後の値
+   */
   convertFromString(value: string) {
     if (URL_REGEX.test(value)) {
       return value;
@@ -16,6 +22,12 @@ export class WordsExcluder implements Convertable {
     return value.replaceAll(this.excludedWordsRegex, '');
   }
 
+  /**
+   * 対象の単語を含む値を取り除いた配列を返却する（URLは処理対象外）
+   * 例）valuesが['abc', 'xyz', 'https://example/ab']、 対象の単語が'ab'の場合、['xyz', 'https://example/ab']を返却する
+   * @param values 変換検証対象の値を含む配列
+   * @returns      対象の単語を含まない値 or 全てのURLを保持する配列
+   */
   convertFromArray(values: string[]) {
     return values.filter(
       (v) => !this.excludedWordsRegex.test(v) || URL_REGEX.test(v),
