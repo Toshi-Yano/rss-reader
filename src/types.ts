42e6b948588f57ab753ce59476fe104da3199d02
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-17 03:53:12 +0900

    [change]Channel, Itemのオプション要素を追記

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+// RSSのみ、Atomは非考慮
 export type Channel = {
   [key: string]: any;
   title: string;
@@ -7,22 +8,22 @@ export type Channel = {
   docs?: string;
   generator?: string;
   items: Item[];
-
-  // language?: string;
-  // copyright?: string;
-  // managingEditor?: string;
-  // webMaster?: string;
-  // pubDate?: string;
-  // cloud?: string;
-  // ttl?: number;
-  // image?: string;
-  // rating?: string;
-  // textInput?: string;
-  // skipHours?: string;
-  // skipDays?: string;
-  // categories?: string[];
+  language?: string;
+  copyright?: string;
+  managingEditor?: string;
+  webMaster?: string;
+  pubDate?: string;
+  cloud?: string;
+  ttl?: number;
+  image?: string;
+  rating?: string;
+  textInput?: string;
+  skipHours?: string;
+  skipDays?: string;
+  categories?: string[];
 };
 
+// RSSのみ、Atomは非考慮
 export type Item = {
   [key: string]: any;
   title?: string;
@@ -32,16 +33,14 @@ export type Item = {
   guid?: string;
   categories?: string[];
   enclosure?: Enclosure;
-
-  // source?: string;
-  // author?: string;
-  // comments?: string;
-
-  // creator?: string;
-  // summary?: string;
-  // content?: string;
-  // isoDate?: string;
-  // contentSnippet?: string;
+  source?: string;
+  author?: string;
+  comments?: string;
+  creator?: string;
+  summary?: string;
+  content?: string;
+  isoDate?: string;
+  contentSnippet?: string;
 };
 
 export type Enclosure = {
@@ -50,22 +49,3 @@ export type Enclosure = {
   length: number;
   type: string;
 };
-
-export type UnknownObject<T extends object> = {
-  [P in keyof T]: unknown;
-};
-
-// export type URL = {
-//   url: string;
-// };
-
-// export type CustomFeed = {
-//   lastBuildDate?: string;
-//   docs?: string;
-//   generator?: string;
-// };
-
-// export type CustomItem = { description?: string };
-
-// export const ParserCustomFeedFields = ['lastBuildDate', 'docs', 'generator'];
-// export const ParserCustomItemFields = ['description'];
