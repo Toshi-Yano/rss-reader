f44368b03b0c36dd625314e379410705e1e969f4
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-18 06:51:45 +0900

    [change]フィード出力用のdescribeメソッドを実装

--- src/feed.ts
+++ src/feed.ts
@@ -1,9 +1,36 @@
 import { Channel } from './types';
 
+const MAX_TEXT_LENGTH_PER_ROW = 80;
+const DELIMITER_LINE_1 = '='.repeat(MAX_TEXT_LENGTH_PER_ROW);
+const DELIMITER_LINE_2 = '-'.repeat(MAX_TEXT_LENGTH_PER_ROW);
+
 export class Feed {
   constructor(public channel: Channel) {}
 
   describe() {
-    return '';
+    const displayValueArray = [
+      DELIMITER_LINE_1,
+      ...this.getChannelFields(),
+      ...this.getItemsFields(),
+    ];
+
+    return displayValueArray.join('\n');
+  }
+
+  private getChannelFields() {
+    return [this.channel.link, this.channel.title, this.channel.description];
+  }
+
+  private getItemsFields() {
+    return this.channel.items.map((item) =>
+      [
+        DELIMITER_LINE_2,
+        item.link,
+        item.pubDate,
+        item.title,
+        item.categories?.join(' / '),
+        item.description,
+      ].join('\n'),
+    );
   }
 }
