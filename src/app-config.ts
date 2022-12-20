b4c8d53031d68f9fe903141fd1a34bc128a16244
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-20 23:43:02 +0900

    [change]除外する単語登録を変更

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,5 +1,5 @@
 // words-excluder.tsで除外する単語
-export const EXCLUDED_WORDS = ['NewsPicks'];
+export const EXCLUDED_WORDS = ['News', 'Blog'];
 
 // rss-parserのOutput(Feed), Item型で明示的に取得する要素名
 export const PARSER_OPTIONS = {
