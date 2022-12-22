937403ab5237f74b4faf9af68aa968a75d80a7a4
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-23 02:46:35 +0900

    [change]Consumableインターフェース追加

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,5 @@
+import { Feed } from './feed';
+
 // フィード取得処理用のインターフェース
 export interface Readable<T> {
   fetchParsedFeeds(): Promise<T[]>;
@@ -7,4 +9,9 @@ export interface Readable<T> {
 export interface Convertable {
   convertFromString(value: string): string;
   convertFromArray(values: string[]): string[];
+}
+
+// 取得したフィードを使用するインターフェース
+export interface Consumable {
+  use(feed: Feed): void;
 }
