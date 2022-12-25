412552863aa3c64dfa5edd21e19a892a99507d1f
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-25 23:00:23 +0900

    [change]ファイル出力をConsumerインターフェースに移譲

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-import * as fs from 'node:fs';
 import { Reader } from './reader';
 import { Convertor } from './convertor';
+import { Consumer } from './consumer';
 import { WordsExcluder } from './words-excluder';
+import { FileWriter } from './file-writer';
 import { Feed } from './feed';
 import { Readable } from './interfaces';
 import { EXCLUDED_WORDS } from './app-config';
@@ -12,39 +13,35 @@ import { EXCLUDED_WORDS } from './app-config';
  * フィードを取得し、convertorが存在する場合は変換処理を行った上で返却する
  * @param reader    フィード取得処理用のインスタンス
  * @param convertor 取得したフィードの変換処理用のインスタンス
+ * @param consumer  取得したフィードを使用するインスタンス
  * @returns         FeedインスタンスのPromise配列
  */
-const readFeeds = async (reader: Readable<Feed>, convertor?: Convertor) => {
+const readFeeds = async (
+  reader: Readable<Feed>,
+  convertor?: Convertor,
+  consumer?: Consumer,
+) => {
   const feeds = await reader.fetchParsedFeeds();
-  convertor &&
-    feeds.forEach((feed) => {
-      convertor.executes(feed.channel);
-    });
+  feeds.forEach((feed) => {
+    convertor && convertor.executes(feed);
+    consumer && consumer.executes(feed);
+  });
   return feeds;
 };
 
 /**
- * 入力されたURLからフィードを取得し、標準出力を行う
- * オプションでフィードの変換処理を行う場合はConvertableインターフェースを実装したインスタンスをConvertorへ登録しておく
- * URLを半角スペースで区切ると複数の入出力が可能
+ * 入力されたURLからフィードを取得し、変換と使用を行う
  */
 (async () => {
   const rl = readline.createInterface({ input, output });
   const inputURLs = await rl.question(
-    '出力したいRSSフィードのURLを入力してください。',
+    '取得したいRSSフィードのURLを入力してください。半角スペースで区切ると複数入力可能です',
   );
 
   const reader = new Reader(inputURLs.split(' '));
   const convertor = new Convertor([new WordsExcluder(EXCLUDED_WORDS)]);
-  const feeds = await readFeeds(reader, convertor);
-  feeds.forEach((feed) => {
-    console.log(feed.describe());
-    try {
-      fs.writeFileSync(`./${feed.channel.title}.txt`, feed.describe());
-      console.log('finish');
-    } catch (error) {
-      console.error(error);
-    }
-  });
+  const consumer = new Consumer([new FileWriter()]);
+
+  await readFeeds(reader, convertor, consumer);
   rl.close();
 })();
